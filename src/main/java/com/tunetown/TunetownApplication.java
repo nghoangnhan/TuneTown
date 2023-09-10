43138165b68b9db964de411d8cd672c7a99f5ffd
Nguyễn Hoàng Nhân <nguyen.hngnhan21@gmail.com>
2023-09-10 14:52:25 +0700

    first commit

---
+++ src/main/java/com/tunetown/TunetownApplication.java
@@ -0,0 +1,13 @@
+package com.tunetown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunetownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunetownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tunetown/TunetownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunetown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunetownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
