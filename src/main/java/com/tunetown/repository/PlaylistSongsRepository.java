b6d7d08f7d93d06a28721975acf75e89248ad62c
Nguyễn Hoàng Nhân <nguyen.hngnhan21@gmail.com>
2023-10-18 21:42:38 +0700

    implement playlist

--- src/main/java/com/tunetown/repository/PlaylistSongsRepository.java
+++ src/main/java/com/tunetown/repository/PlaylistSongsRepository.java
@@ -4,7 +4,12 @@ import com.tunetown.model.PlaylistSongs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface PlaylistSongsRepository extends JpaRepository<PlaylistSongs, Integer> {
     @Query("SELECT COUNT(ps) FROM PlaylistSongs ps WHERE ps.playlist.id = ?1")
     int getNumberOfSongsInPlaylist(int playlistId);
+
+    @Query("SELECT ps FROM PlaylistSongs  ps where ps.playlist.id = ?1")
+    List<PlaylistSongs> getPlaylistSongsById(int playlistId);
 }
