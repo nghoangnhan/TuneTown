624be1503b084da428ba8b677f7c25f8e2523a30
Nguyễn Hoàng Nhân <nguyen.hngnhan21@gmail.com>
2023-10-21 10:45:59 +0700

    configure ExceptionHandler

--- src/main/java/com/tunetown/handler/GlobalExceptionHandler.java
+++ src/main/java/com/tunetown/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.tunetown.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,5 +12,11 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public ResponseStatusException handleResponseStatusException(ResponseStatusException ex) {
         return new ResponseStatusException(ex.getStatusCode(), ex.getReason());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseStatusException handleException(Exception ex) {
+        return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 }
