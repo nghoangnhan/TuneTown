a9f6472f87c34f97a7ff90e80e8edb0d95dfac71
lhlamlehoang <lhlam.lehoang@gmail.com>
2024-05-30 20:50:41 +0700

    hot fix

--- src/main/java/com/tunetown/repository/PostRepository.java
+++ src/main/java/com/tunetown/repository/PostRepository.java
@@ -8,11 +8,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     @Query("SELECT p FROM Post p")
     Page<Post> getAllPosts(Pageable pageable);
 
     @Query("SELECT p FROM Post p WHERE p.author.id = ?1")
-    Page<Post> getPostByAuthorId(int authorId, Pageable pageable);
+    Page<Post> getPostByAuthorId(UUID authorId, Pageable pageable);
 }
